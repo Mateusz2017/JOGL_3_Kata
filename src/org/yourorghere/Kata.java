19095924ab6ebdd5f8bd34fb69ea215628826c96
student <student@240-02.INFORMATYKA.local>
2016-10-06 14:06:00 +0200

    zad 4b - kolko

--- src/org/yourorghere/Kata.java
+++ src/org/yourorghere/Kata.java
@@ -126,8 +126,8 @@ public class Kata implements GLEventListener {
         for(kat = 0.0f; kat < (2.0f*Math.PI);
         kat+=(Math.PI/32.0f))
         {
-        x = 1.5f*(float)Math.sin(kat);
-        y = 1.5f*(float)Math.cos(kat);
+        x = 2.2f*(float)Math.sin(kat);
+        y = 2.2f*(float)Math.cos(kat);
         gl.glVertex3f(x, y, -6.0f); //kolejne punkty
         }
         gl.glEnd();
